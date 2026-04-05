2a1b69216395e58d9126881213bee6cc242bc845
darius <saladarius72@gmail.com>
2026-04-05 20:47:21 +0300

    repairing web

--- music-player-frontend/web/p2p-worker.js
+++ music-player-frontend/web/p2p-worker.js
@@ -7,7 +7,7 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => event.waitUntil(self.clients.claim()));
 
 self.addEventListener('message', (event) => {
-    if (event.data && event.data.typgoe === 'SKIP_WAITING') {
+    if (event.data && event.data.type === 'SKIP_WAITING') {
         self.skipWaiting();
         return;
     }
